d9b92a285b7d5704e097c4db3cd2096c9ae07169
russtack <stirlingest@gmail.com>
2023-04-18 20:47:10 -0400

    new

---
+++ src/pages/buttonPage.js
@@ -0,0 +1,42 @@
+import { GoBell, GoDatabase, GoCloudDownload } from "react-icons/go";
+
+import Button from "../components/Button";
+import "./index.css";
+
+function ButtonPage() {
+  const handleClick = () => {
+    console.log("buton presed");
+  };
+  return (
+    <div>
+      <div>
+        <Button success rounded outline className="mb-5">
+          <GoBell /> Clicke mich!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <GoDatabase />
+          Heyyyy
+        </Button>
+      </div>
+      <div>
+        <Button warning outline>
+          <GoCloudDownload /> Supppp
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Blah
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Brut
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
