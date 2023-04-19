49aac810002d89155d2fcaddaec86cfb420a1fb4
russtack <stirlingest@gmail.com>
2023-04-18 21:41:33 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import Accordion from
+import Accordion from "./components/Accordion";
 import "./index.css";
 
 function App() {
+  const items = [
+    {
+      id: "w2",
+      label: "Can I use React?",
+      content: "Yes",
+    },
+    {
+      id: "w3",
+      label: "Can i use JS",
+      content: "U do",
+    },
+    {
+      id: "w36",
+      label: "Can I use AI",
+      content: "yeah",
+    },
+  ];
+
+  return <Accordion items={items} />;
 }
 
 export default App;
