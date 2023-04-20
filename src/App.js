dd59771b246cda0eabc6e1ffc914038879e5f57c
russtack <stirlingest@gmail.com>
2023-04-20 16:31:18 -0400

    Panel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Dropdown from "./components/Dropdown";
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [selection, setSelection] = useState(null);
+
   const options = [
     { label: "Red", value: "red" },
     { label: "Green", value: "green" },
     { label: "Blue", value: "blue" },
   ];
-  return <Dropdown options={options} />;
+
+  const handleSelection = (option) => {
+    setSelection(option);
+    console.log(`selected:${selection}`);
+  };
+  return (
+    <Dropdown value={selection} onChange={handleSelection} options={options} />
+  );
 }
 
 export default App;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,11 +1,44 @@
-function Dropdown({ options }) {
+import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+
+function Dropdown({ options, value, onChange }) {
+  const [isOpen, setIsopen] = useState(false);
+
+  const handleClick = () => {
+    setIsopen(!isOpen);
+  };
+  const handleOptionClick = (option) => {
+    setIsopen(false);
+
+    onChange(option);
+  };
+
   const renderedOptions = options.map((option) => {
-    return <div key={option.value}>{option.label}</div>;
+    return (
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        key={option.value}
+        onClick={() => handleOptionClick(option)}
+      >
+        {option.label}
+      </div>
+    );
   });
+
   return (
-    <div>
-      <div>Select...</div>
-      <div>{renderedOptions}</div>
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
+      </div>
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 }
