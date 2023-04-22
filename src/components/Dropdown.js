808d0490c318a7b9e251b1450cb6c7b407bf348a
russtack <stirlingest@gmail.com>
2023-04-21 22:28:04 -0400

    edit

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { GoChevronDown } from "react-icons/go";
 import Panel from "./Panel";
 
 function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsopen] = useState(false);
+  const divEl = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (divEl.current.contains(event.target)) {
+      }
+    };
+    document.addEventListener("click", handler, true);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
 
   const handleClick = () => {
     setIsopen(!isOpen);
@@ -27,7 +38,7 @@ function Dropdown({ options, value, onChange }) {
   });
 
   return (
-    <div className="w-48 relative">
+    <div ref={divEl} className="w-48 relative">
       <Panel
         className="flex justify-between items-center cursor-pointer"
         onClick={handleClick}
