f5144ad2fc4f0b47249d3c19568e809ac59e0021
russtack <stirlingest@gmail.com>
2023-04-22 20:03:50 -0400

    navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,5 @@
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  const handleSelection = (option) => {
-    setSelection(option);
-    console.log(`selected:${selection}`);
-  };
-  return (
-    <Dropdown value={selection} onChange={handleSelection} options={options} />
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/pages/DropDownPage.js
@@ -0,0 +1,22 @@
+import Dropdown from "../components/Dropdown";
+import { useState } from "react";
+
+function DropDownPage() {
+  const [selection, setSelection] = useState(null);
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+
+  const handleSelection = (option) => {
+    setSelection(option);
+    console.log(`selected:${selection}`);
+  };
+  return (
+    <Dropdown value={selection} onChange={handleSelection} options={options} />
+  );
+}
+
+export default DropDownPage;
