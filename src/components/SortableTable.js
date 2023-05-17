73c04ca1d625b672422283c17de336c8e55c2f61
russtack <stirlingest@gmail.com>
2023-05-17 01:44:22 -0400

    sort SortableTable

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
 import Table from "./Table";
 
 function SortableTable(props) {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
   const { config, data } = props;
+
   const handleClick = (label) => {
-    if (setSortOrder === null) {
+    if (sortBy && label !== sortBy) {
+      setSortOrder("asc");
+      setSortBy(label);
+    }
+    if (sortOrder === null) {
       setSortOrder("asc");
       setSortBy(label);
     } else if (sortOrder === "asc") {
@@ -26,11 +32,15 @@ function SortableTable(props) {
       ...column,
       header: () => (
         <th
+          className="cursor-pointer hover:bg-slate-400"
           onClick={() => {
             handleClick(column.label);
           }}
         >
-          {column.label} ES SOrTABLE
+          <div className="flex items-center">
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
         </th>
       ),
     };
@@ -50,6 +60,37 @@ function SortableTable(props) {
     });
   }
   return <Table {...props} data={sortedData} config={updatedConfig} />;
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
+  }
+  if (sortOrder === null) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
+  } else if (sortOrder === "asc") {
+    return (
+      <div>
+        <GoArrowSmallUp />
+      </div>
+    );
+  } else if (sortOrder === "desc") {
+    return (
+      <div>
+        <GoArrowSmallDown />
+      </div>
+    );
+  }
 }
 
 export default SortableTable;
